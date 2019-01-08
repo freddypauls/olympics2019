b5b6a5ecbcba7d6c9dca4ade641592edbe617143
Fredrik <paasan-fred@hotmail.com>
2019-01-08 15:08:32 +0100

    trying to fix single user print
    this thing is kind of stupid, because if sentence wont work inside JSX so I kinda have to jerryrig something that will work but wont be the best solution to be honest.
    I@ll probbably fix something better eventually, but have done alot on this at the moment and I just really want to move on. if you actually read this then cudos to you sir, but I have to be done with it now so goodbye and see you around another time in  another text ;)

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,28 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
 import app, { auth } from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import '../Firebase';
+import { compose } from 'recompose';
 
 import { AuthUserContext, withAuthorization } from '../Session';
-import { PasswordForgetForm } from '../PasswordForget';
-import PasswordChangeForm from '../PasswordChange';
+import { PasswordForgetForm } from '../PasswordForget'; //Might need
+import PasswordChangeForm from '../PasswordChange'; //Might need
 import '../SignIn/index.css';
 import './index.css';
-import { userInfo } from 'os';
+import { userInfo } from 'os'; //Might need later
 
-const AccountPage = () => (
+class AccountPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+
+    this.props.firebase.users().on('value', snapshot => {
+      const usersObject = snapshot.val();
+
+      const usersList = Object.keys(usersObject).map(key => ({
+        ...usersObject[key],
+        uid: key,
+      }));
+
+      this.setState({
+        users: usersList,
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.users().off();
+  }
+
+  render() {
+    const { users } = this.state;
+
+    return (
+      <div>
+        <AccountInfo users={users}/>
+      </div>
+    );
+  }
+}
+const AccountInfo = ({users}) => (
     <AuthUserContext.Consumer>
       {authUser => (
         <div>
+        {users.map(user => (
           <div className="form-card form-card-account">
-            <h1>Account: </h1>
-            <p>Email: {authUser.email}</p>
-            <p>Username: </p>
+            <p key={user.uid}>
+                {user.uid === authUser.uid ? <div><strong>Username: </strong> {user.username}
+                <br/>
+                <strong>Email:</strong> {user.email}</div> : null} 
+            </p>
           </div>
+          ))};
         </div>
       )}
     </AuthUserContext.Consumer>
+  );
+
+  const UserList = ({users}) => (
+    <div>
+      {users.map(user => (
+        <ul key={user.uid}>
+          <li>
+            <strong>Username: </strong> {user.username}
+            {user.roles ? <p><strong>Role: </strong> {user.roles} </p> : <p><strong>Role: </strong> no role</p>} 
+          </li>
+        </ul>
+      ))};
+    </div>
+
   );
 
 // <PasswordForgetForm />
